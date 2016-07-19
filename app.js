4290bf21539e3b42c34c5495b618afc3676b6689
Shalin <shalinpatel75@gmail.com>
2016-07-19 00:52:14 -0400

    Changed app to serve view based on request url

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var port = process.env.PORT || 3000; // necessary for heroku
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', function(req,res) {
-	res.render('pages/index');
+app.get('/*', function(req,res) {
+	var view = req.url === '/' ? 'index' : req.url;
+	console.log(view);
+	res.render( path.join('pages/', view) );
 });
 
 app.listen(port,function() {
