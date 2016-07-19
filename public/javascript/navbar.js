319fae9af0858a46e87d8995722bfcaea2f60ea6
Shalin <shalinpatel75@gmail.com>
2016-07-19 14:53:44 -0400

    Setup javascript to add class 'active' to current navbar page

--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -1,3 +1,17 @@
 $( document ).ready( function() {
-	console.log('ready!');
+
+	initActiveNavLink();
+
 });
+
+// add the class active to the current page
+function initActiveNavLink(){
+	// match first / and all non-slash chars after
+	var current_nav_link = $(location)[0].pathname.match(/\/[^/]+|\//)[0];
+	var current_nav_jquery_selector = ".navbar__entry[href='"
+		+ current_nav_link
+		+ "']"
+	;
+	$( current_nav_jquery_selector ).addClass('active');
+
+}
